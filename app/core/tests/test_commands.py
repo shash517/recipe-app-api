a2376b894609adcb76af585675660b70a79905b8
shash <shash_shukla@yahoo.com>
2025-05-23 15:01:08 +0100

    fixing lint issue5

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -3,11 +3,9 @@ Test custom Django management commands.
 """
 from unittest.mock import patch
 from psycopg2 import OperationalError as Psycopg2OpError
-
 from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
-
 
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
@@ -21,7 +19,6 @@ class CommandTests(SimpleTestCase):
 
         patched_check.assert_called_once_with(databases=['default'])
     
-
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """Test waiting for database when getting OperationalError."""
@@ -32,4 +29,3 @@ class CommandTests(SimpleTestCase):
 
         self.assertEqual(patched_check.call_count, 6)
         patched_check.assert_called_with(databases=['default'])
-         
